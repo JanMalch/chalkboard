b9b3c42bcb727b3345044649f98a8b9e6ab1fede
JanMalch <25508038+JanMalch@users.noreply.github.com>
2026-02-22 11:59:50 +0100

    feat: add icons and PWA manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import './layout.scss';
-	import favicon from '$lib/assets/favicon.svg';
 	import { version } from '$app/environment';
 
 	let { children } = $props();
@@ -8,7 +7,6 @@
 
 <svelte:head>
 	<title>Chalkboard</title>
-	<link rel="icon" href={favicon} />
 	<meta content="v{version}" name="version" />
 </svelte:head>
 {@render children()}
